7e98e8439db5d337b56b357f177f6736f1415ad3
v833 <2507301541@qq.com>
2022-01-21 00:11:39 +0800

    day-01

---
+++ type-challenges/4-easy-pick/index.ts
@@ -0,0 +1,30 @@
+/* 
+Implement the built-in Pick<T, K> generic without using it.
+
+Constructs a type by picking the set of properties K from T
+*/
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+const todo: TodoPreview = {
+  title: 'Clean room',
+  completed: false,
+}
+
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+
+// js 实现
+// function myPick(T, K) {
+//   const obj = {}
+//   K.forEach((key) => {
+//     if (key in T) obj[key] = T[key]
+//   })
+//   return obj
+// }
