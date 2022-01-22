e32640f16a5f126a21bb1f8aa508b515b5ee6220
v833 <2507301541@qq.com>
2022-01-23 01:28:45 +0800

    day-02

--- type-challenges/easy/11-tuple-to-object/index.ts
+++ type-challenges/easy/11-tuple-to-object/index.ts
@@ -1,7 +1,9 @@
+// 完整的对象键只能是string、number、symbol
 type TupleToObject<T extends readonly (string | symbol | number)[]> = {
   [P in T[number]]: P
 }
 const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
 
+// keyof array 得到索引
 type result = TupleToObject<typeof tuple>
 // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
